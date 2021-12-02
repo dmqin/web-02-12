ed10a039627e957702113eb4a117a87ecf12746f
dmqin <dimqin2004@gmail.com>
2021-12-02 22:45:36 +0700

    fix bug

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,7 @@ import 'tailwindcss/tailwind.css'
 import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
  <Head>
-<link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+
 
 
  </Head>
